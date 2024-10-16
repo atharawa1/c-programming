168e9c10b27a3c94f6c8cac0627492dffcf78824
atharawa1 <atharawa@eng.rizvi.edu.in>
2024-10-16 09:39:56 +0530

    Add files via upload

---
+++ Experiment1 Task 1.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+
+ int main()
+{
+ printf("Name\t: Atharawa Prasad Parulekar\n");
+ printf("roll.no\t: 60\n");
+ printf("div\t: E\n");
+ printf("age\t: 18 \n");
+ printf("UIN\t: 241S078\n");
+ printf("class\t: ECS\n");
+
+ return 0;
+ }
